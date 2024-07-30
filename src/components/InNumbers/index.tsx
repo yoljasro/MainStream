46020c8a9fb74e6801ec2f7d6417bb978057a655
yoljasro <saidaliyevjasur450@gmail.com>
2024-07-30 17:35:37 +0500

    changes

--- src/components/InNumbers/index.tsx
+++ src/components/InNumbers/index.tsx
@@ -33,21 +33,21 @@ export const InNumbers = () => {
                     <p className={styles.numbers__number}>25+</p>
                     <p className={styles.numbers__desc}>{t("statistic.employees")}</p>
                 </div>
-                <div className={styles.numbers__cardone}>
-                    <p>200K </p>
-                    <p>{t("statistic.lead")}</p>
+                <div className={styles.numbers__cardsecond}>
+                    <p className={styles.numbers__numbertwo}>200K </p>
+                    <p className={styles.numbers__desctwo}>{t("statistic.lead")}</p>
                 </div>
-                <div className={styles.numbers__card}>
-                    <p>1,126  МЛРД </p>
-                    <p>{t("statistic.videos")}</p>
+                <div className={styles.numbers__cardthird}>
+                    <p className={styles.numbers__numbertwo}>1,126 <span className={styles.numbers__span}>МЛРД</span>  </p>
+                    <p className={styles.numbers__desctwo}>{t("statistic.videos")}</p>
                 </div>
-                <div className={styles.numbers__card}>
-                    <p>3,49 МЛН </p>
-                    <p>{t("statistic.followers")}</p>
+                <div className={styles.numbers__cardfourt}>
+                    <p className={styles.numbers__numbertwo}>3,49 <span className={styles.numbers__span}>МЛН</span>  </p>
+                    <p className={styles.numbers__desctwo}>{t("statistic.followers")}</p>
                 </div>
-                <div className={styles.numbers__card}>
-                    <p>3,49 МЛН </p>
-                    <p>{t("statistic.cost")}</p>
+                <div className={styles.numbers__cardfive}>
+                    <p className={styles.numbers__numberlast}>0,01  $  </p>
+                    <p className={styles.numbers__desclast}>{t("statistic.cost")}</p>
                 </div>
             </div>
         </div>
