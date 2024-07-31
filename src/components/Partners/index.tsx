14ac3e591928acf8bc28900111c0758a541c2625
yoljasro <saidaliyevjasur450@gmail.com>
2024-07-31 12:42:09 +0500

    changs

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -3,6 +3,8 @@ import styles from './index.module.sass';
 import { useRouter } from "next/router";
 import { useTranslations } from "next-intl";
 import Image from 'next/image';
+import { FaInstagram } from "react-icons/fa6";
+
 
 export const Partners = () => {
     const t = useTranslations()
@@ -13,6 +15,14 @@ export const Partners = () => {
           <div className={styles.partners__carousel}>
             <div className={styles.partners__slideone}></div>
             <div className={styles.partners__slidetwo}></div>
+          </div>
+          <p className={styles.partners__desc}>Ценности нашей компании это долгосрочное и плодотворное партнёрство.</p>
+          <div className={styles.partners__section}>
+          <Image src={'/assets/img/logoorg.png'} alt='logo' width={301} height={27}/>
+          <div className={styles.partners__line}></div>
+          <div className={styles.partners__insta}>
+          <FaInstagram className={styles.partners__icon} />
+          </div>
           </div>
         </div>
     )
