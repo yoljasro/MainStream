5773da92d0bfeb3b74dc7462b60f6f84ad7e748f
yoljasro <saidaliyevjasur450@gmail.com>
2024-08-01 18:21:12 +0500

    changes

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import styles from './index.module.sass';
 import Image from 'next/image';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 interface FormData {
   name: string;
@@ -37,34 +39,108 @@ export const Form: React.FC = () => {
         <div className={styles.info}>
           <p className={styles.info__title}>Остались вопросы?</p>
           <p className={styles.info__description}>Оставьте контактные данные, наши менеджеры свяжутся с Вами и проконсультируют</p>
-          <Image className={styles.info__logo} src={'/assets/img/logoorg.png'} alt='logoorg' width={356} height={32}/>
+          <Image className={styles.info__logo} src={'/assets/img/logoorg.png'} alt='logoorg' width={356} height={32} />
         </div>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <input  
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            value={formData.name}
-            onChange={handleChange}
+          <TextField
+            name='name'
             className={styles.form__input}
+            onChange={handleChange}
+            value={formData.name}
+            id="standard-basic" label="Руслан Мариарти"
+            variant="standard"
+            sx={{
+              '& .MuiInput-underline:before': {
+                borderBottom: '1px solid #FFFFFF', // Chiziq rangi oq
+              },
+              '& .MuiInputLabel-root': {
+                color: '#5B539D',
+                fontSize: "28px",
+                fontFamily: "Arial, Helvetica, sans-serif"
+              },
+              width: '100%',
+              maxWidth: '526px',
+              '.MuiInputBase-root': {
+                borderLeft: 'none',
+                borderRight: 'none',
+                borderBottom: 'none',
+                outline: 'none',
+                color: '#FFFFFF !important',
+                paddingLeft: '85px',
+                fontSize: '24px !important',
+                borderColor: '#FFFFFF ',
+                outlineColor: '#FFFFFF',
+              },
+            }}
           />
-          <input
+
+          <TextField
             type="email"
             name="email"
-            placeholder="Your Email"
             value={formData.email}
             onChange={handleChange}
             className={styles.form__input}
+            id="standard-basic"
+            label="+998 (90) 024 44 48"
+            variant="standard"
+            sx={{
+              '& .MuiInput-underline:before': {
+                borderBottom: '1px solid #FFFFFF', // Chiziq rangi oq
+              },
+              '& .MuiInputLabel-root': {
+                color: '#5B539D',
+                fontSize: "28px",
+                fontFamily: "Arial, Helvetica, sans-serif"
+              },
+              width: '100%',
+              maxWidth: '526px',
+              '.MuiInputBase-root': {
+                borderLeft: 'none',
+                borderRight: 'none',
+                borderBottom: 'none',
+                outline: 'none',
+                color: '#FFFFFF !important',
+                paddingLeft: '85px',
+                fontSize: '24px !important',
+                borderColor: '#FFFFFF ',
+                outlineColor: '#FFFFFF',
+              },
+            }}
           />
-          <input
+          <TextField
             type="tel"
             name="phone"
-            placeholder="Your Phone Number"
             value={formData.phone}
             onChange={handleChange}
             className={styles.form__input}
+            id="standard-basic"
+            label="Название компании"
+            variant="standard"
+            sx={{
+              '& .MuiInput-underline:before': {
+                borderBottom: '1px solid #FFFFFF', // Chiziq rangi oq
+              },
+              '& .MuiInputLabel-root': {
+                color: '#5B539D',
+                fontSize: "28px",
+                fontFamily: "Arial, Helvetica, sans-serif",
+              },
+              width: '100%',
+              maxWidth: '526px',
+              '.MuiInputBase-root': {
+                borderLeft: 'none',
+                borderRight: 'none',
+                borderBottom: 'none',
+                outline: 'none',
+                color: '#FFFFFF !important',
+                paddingLeft: '85px',
+                fontSize: '24px !important',
+                borderColor: '#FFFFFF ',
+                outlineColor: '#FFFFFF',
+              },
+            }}
           />
-          <button type="submit" className={styles.form__submit}>Submit</button>
+          <Button variant='contained' type="submit" className={styles.form__submit}>Отправить</Button>
         </form>
       </div>
     </div>
