a9f94e2785dd0011e592ccc7f22458afd3837c2a
yoljasro <saidaliyevjasur450@gmail.com>
2024-08-08 16:10:18 +0500

    changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,15 +13,6 @@ export const Footer = () => {
         <p className={styles.footer__number}>+998 90 024 44 48</p>
         <p className={styles.footer__email}>mainstream.uz@gmail.com</p>
         <p className={styles.footer__adress}>{t("footer.adress")}</p>
-        {/* <div className={styles.footer__icons}>
-          <div className={styles.footer__circle}></div>
-          <Image src={'/assets/img/youtube.png'} alt='youtube' width={35} height={35}/>
-          <Image src={'/assets/img/telegram.png'} alt='youtube' width={35} height={35}/>
-          <Image src={'/assets/img/whatsapp.png'} alt='youtube' width={35} height={35}/>
-          <Image src={'/assets/img/instagram.png'} alt='youtube' width={35} height={35}/>
-          <Image src={'/assets/img/facebook.png'} alt='youtube' width={35} height={35}/>
-
-        </div>  */}
       </div>
 
       <div className={styles.footer__social}>
